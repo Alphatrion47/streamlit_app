b24b50dba865b83f730984548aab00c16f76250f
R Srinath <97286288+Alphatrion47@users.noreply.github.com>
2024-08-08 15:29:07 +0530

    Update my_groq.py
    API key kept as streamlit secrets

--- my_groq.py
+++ my_groq.py
@@ -4,7 +4,7 @@ from groq import Groq
 import streamlit as st
 
 
-client = Groq(api_key=os.environ.get('groq_pass'))
+client = Groq(api_key= st.secrets["groq_pass"])
 
 st.set_page_config(
     page_title="Dataset Analyzer",
